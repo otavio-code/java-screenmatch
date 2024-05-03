5497b265e118831ede24b9d2a00dd974686959f4
Otavio Andrade <otavio.sto@gmail.com>
2024-05-02 22:25:21 -0300

    feat: trabalhando com textos

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,17 @@ public class Main {
         System.out.println("Ano do lançamento: " + anoDeLancamento);
         boolean incluidoNoPlano = true;
         double notaDoFIlme = 8.1;
+        // Média calculada pelas 3 notas
+        double media = (9.8 + 6.3 + 8) / 3;
+        System.out.println("Media: " + media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com o galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
 
     }
 }
