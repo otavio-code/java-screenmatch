caa833332eb0025542d1ad3e7ce2e38d316ca883
Otavio Andrade <otavio.sto@gmail.com>
2024-05-02 22:14:24 -0300

    feat: criando variaveis

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alô Mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano do lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFIlme = 8.1;
+
     }
 }
