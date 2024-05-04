ae101553a4b7e4da3ed65330adeddee33ec734d6
Otavio Andrade <otavio.sto@gmail.com>
2024-05-04 20:33:04 -0300

    feat: alterado a forma de expor a mensagem de resultado

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,9 @@ public class Main {
         Scanner ler = new Scanner(System.in);
         temperatura_celsius = ler.nextDouble();
         temperatura_fahrenheit = (temperatura_celsius * 1.8) + 32;
-        System.out.println("A temperatura em farenheit: " + temperatura_fahrenheit);
+
+        String mensagem = String.format("A temperatura em graus celsius %f é equivalente a temperatura %f em farenheit: ", temperatura_celsius, temperatura_fahrenheit);
+        System.out.println(mensagem);
 
     }
 }
