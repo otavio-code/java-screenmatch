979dbfe9881ecb7cf9df3f09f60b3922ce7efd92
Otavio Andrade <otavio.sto@gmail.com>
2024-05-04 20:24:23 -0300

    feat: desafio de conversao de celsius para fahrenheit concluído com sucesso

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Esse é o Screen Match");
-        System.out.println("Filme: Top Gun: Maverick");
+        System.out.println("Informe a temperatura em graus Celsius para converter em Fahrenheit: ");
+        double temperatura_fahrenheit;
+        double temperatura_celsius;
+        Scanner ler = new Scanner(System.in);
+        temperatura_celsius = ler.nextDouble();
+        temperatura_fahrenheit = (temperatura_celsius * 1.8) + 32;
+        System.out.println("A temperatura em farenheit: " + temperatura_fahrenheit);
 
-        int anoDeLancamento = 2022;
-        System.out.println("Ano do lançamento: " + anoDeLancamento);
-        boolean incluidoNoPlano = true;
-        double notaDoFIlme = 8.1;
-        // Média calculada pelas 3 notas
-        double media = (9.8 + 6.3 + 8) / 3;
-        System.out.println("Media: " + media);
-        String sinopse;
-        sinopse = """
-                Filme Top Gun
-                Filme de aventura com o galã dos anos 80
-                Muito bom!
-                Ano de lançamento
-                """ + anoDeLancamento;
-        System.out.println(sinopse);
-
-        //casting explicíto na variável classficacao, transformando o valor que está recebendo em um inteiro
-        int classificacao = (int) (media / 2);
-        System.out.println(classificacao);
     }
 }
