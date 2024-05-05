2296c5d2726c9a836068a112ef98faa1c7bfd742
Otavio Andrade <otavio.sto@gmail.com>
2024-05-04 21:13:48 -0300

    feat: realizado novos exercícios

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,40 @@ public class Main {
         var1 = 3.434;
         var2 = (int) (var1);
         System.out.println("Imprimindo a variavel " + var2);
+
+        /*Declare uma variável do tipo double precoProduto e uma variável
+         do tipo int (quantidade). Calcule o valor total multiplicando o preço
+          do produto pela quantidade e apresente o resultado em uma mensagem.
+         */
+        double precoProduto = 5.49;
+        int quantidade = 8;
+        double resultado = precoProduto * quantidade;
+        mensagem = String.format("O valor do produto é R$ %f", resultado);
+        System.out.println(mensagem);
+
+        /*Declare uma variável do tipo double valorEmDolares. Atribua um valor
+         em dólares a essa variável. Considere que o valor de 1 dólar é equivalente
+          a 4.94 reais. Realize a conversão do valor em dólares para reais
+           e imprima o resultado formatado.
+         */
+
+        double valorEmDolares = 10;
+        double resultadoEmReais = valorEmDolares * 4.94;
+        mensagem = String.format("O resultado é R$ %.2f", resultadoEmReais);
+        System.out.println(mensagem);
+
+        /*Declare uma variável do tipo double precoOriginal.
+         Atribua um valor em reais a essa variável, representando o preço original
+          de um produto. Em seguida, declare uma variável do tipo double percentualDesconto
+           e atribua um valor percentual de desconto ao produto (por exemplo, 10 para 10%).
+            Calcule o valor do desconto em reais, aplique-o
+             ao preço original e imprima o novo preço com desconto.
+         */
+        double precoOriginal = 15.89;
+        double percentualDesconto = 10;
+        double descontoAplicado = (precoOriginal * percentualDesconto) /100;
+        double resultadoPreco = (precoOriginal - descontoAplicado);
+        mensagem = String.format("O desconto aplicado será de R$ %.2f, o novo valor será de R$ %.2f", descontoAplicado, resultadoPreco);
+        System.out.println(mensagem);
     }
 }
